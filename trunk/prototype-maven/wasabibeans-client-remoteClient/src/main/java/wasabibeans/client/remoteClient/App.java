81622e3d6f634ba76dc292985c8e41705099f841
schulte@uni-paderborn.de <schulte@uni-paderborn.de@4fd6c35a-9cae-1b4d-042b-f8107d73f882>
2010-05-25 16:03:13 +0000

    initial commit with maven

---
+++ trunk/prototype-maven/wasabibeans-client-remoteClient/src/main/java/wasabibeans/client/remoteClient/App.java
@@ -0,0 +1,13 @@
+package wasabibeans.client.remoteClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
