6121f28953bb5d02981e9a534aeecd7a750974fa
d.klaholt <d.klaholt@gmx.de>
2010-05-25 09:05:51 +0000

    initial

---
+++ prototype/core/ejbModule/jackrabbit/ConnectionTest.java
@@ -0,0 +1,47 @@
+package jackrabbit;
+
+import javax.ejb.Stateful;
+import javax.jcr.Credentials;
+import javax.jcr.Node;
+import javax.jcr.Repository;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.nodetype.NodeType;
+import javax.naming.InitialContext;
+
+import org.wasabibeans.server.core.transfer.model.dto.WasabiRoomDTO;
+
+/**
+ * Session Bean implementation class ConnectionTest
+ */
+@Stateful
+public class ConnectionTest implements ConnectionTestRemote {
+	
+	/**
+	 * Default constructor.
+	 */
+	public ConnectionTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public WasabiRoomDTO login() {
+		try {
+			InitialContext ctx = new InitialContext();
+			Repository rep = (Repository) ctx.lookup("java:jcr/local");
+			Credentials cred = new SimpleCredentials("user", new char[] { 'p',
+					'w', 'd' });
+			Session s = rep.login(cred);
+
+			Node rootNode = s.getRootNode();
+			Node test = rootNode.addNode("test");
+			test.setPrimaryType(NodeType.NT_UNSTRUCTURED);
+			s.save();
+			
+			WasabiRoomDTO root = new WasabiRoomDTO();
+			root.setId(test.getIdentifier());
+			return root;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
