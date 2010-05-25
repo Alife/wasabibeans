0500909ab6fb3965b60ebd402620bdb9985afe0f
d.klaholt <d.klaholt@gmx.de>
2010-05-25 09:11:28 +0000

    initial

---
+++ prototype/remoteClient/src/Test.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+
+public class Test {
+	public static void main(String[] horst) {
+		try {
+			byte buffer[] = new byte[80];
+			int read;
+			read = System.in.read(buffer, 0, 80);
+			String input = new String(buffer, 0, read);
+			String[] tmp = input.split("\r");
+			System.out.println(tmp[0]);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
