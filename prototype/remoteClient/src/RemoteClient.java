bae52309be5612c24b7e47684b5f4aa340600732
d.klaholt <d.klaholt@gmx.de>
2010-06-17 16:21:02 +0000

    added the jboss ClientLoginModule to the remote client authentication on the server does not yet work, though - for now any user can login with any password

--- prototype/remoteClient/src/RemoteClient.java
+++ prototype/remoteClient/src/RemoteClient.java
@@ -1,9 +1,15 @@
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.security.auth.login.AppConfigurationEntry;
+import javax.security.auth.login.Configuration;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag;
 
 import de.wasabibeans.framework.server.core.dto.WasabiDocumentDTO;
 import de.wasabibeans.framework.server.core.dto.WasabiRoomDTO;
@@ -11,28 +17,40 @@ import de.wasabibeans.framework.server.core.jcrTest.ConnectionTestRemote;
 import de.wasabibeans.framework.server.core.remote.DocumentServiceRemote;
 
 public class RemoteClient {
-	public static void main(String[] horst) throws NamingException {
+	public static void main(String[] horst) throws NamingException, LoginException {
+		// get the jndi context
 		Properties properties = new Properties();
-		properties.put(Context.INITIAL_CONTEXT_FACTORY,
-				"org.jnp.interfaces.NamingContextFactory");
-		properties.put(Context.URL_PKG_PREFIXES,
-				"org.jboss.naming:org.jnp.interfaces");
+		properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory"); // "org.jboss.security.jndi.JndiLoginInitialContextFactory");
+		properties.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
 		properties.put(Context.PROVIDER_URL, "localhost:1099");
-		Context context = new InitialContext(properties);
-		
-		/*Test1Remote test1 = (Test1Remote) context.lookup("Test1/remote");
-		Test2Remote test2 = (Test2Remote) context.lookup("Test2/remote");
-		
-		print(test1.get());
-		test1.set("ufftata");
-		print(test1.get());
-		
-		
-		print(test2.get());*/
+		// properties.put(Context.SECURITY_PRINCIPAL, "root");
+		// properties.put(Context.SECURITY_CREDENTIALS, "horst");
+		Context jndiContext = new InitialContext(properties);
 
-		ConnectionTestRemote bean = (ConnectionTestRemote) context
-				.lookup("wasabibeans/ConnectionTest/remote");
-		DocumentServiceRemote documentService = (DocumentServiceRemote) context
+		// set a jaas configuration
+		Configuration.setConfiguration(new Configuration() {
+			public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
+				HashMap<String, String> options = new HashMap<String, String>();
+
+				return new AppConfigurationEntry[] { new AppConfigurationEntry("org.jboss.security.ClientLoginModule",
+						LoginModuleControlFlag.REQUIRED, options) };
+
+			}
+
+			public void refresh() {
+
+			}
+		});
+
+		// authenticate via jaas (that is, use ClientLoginModule of jboss which does not really authenticate ->
+		// authentication must happen on the server)
+		// name parameter doesn't matter due to use of anonymous class above
+		LoginContext loginContext = new LoginContext("doesntMatter", new ConsoleCallbackHandler());
+		loginContext.login();
+
+		// use the ejbs remotely
+		ConnectionTestRemote bean = (ConnectionTestRemote) jndiContext.lookup("wasabibeans/ConnectionTest/remote");
+		DocumentServiceRemote documentService = (DocumentServiceRemote) jndiContext
 				.lookup("wasabibeans/DocumentService/remote");
 		try {
 			String input = "";
@@ -61,8 +79,7 @@ public class RemoteClient {
 					}
 				} else if (input.equals("show")) {
 					System.out.println("=====Alle Dokumente=====");
-					for (WasabiDocumentDTO doc : documentService
-							.getDocuments(root)) {
+					for (WasabiDocumentDTO doc : documentService.getDocuments(root)) {
 						System.out.print(documentService.getName(doc) + ": ");
 						try {
 							System.out.println(documentService.getContent(doc));
@@ -93,7 +110,7 @@ public class RemoteClient {
 			return "";
 		}
 	}
-	
+
 	private static void print(String string) {
 		System.out.println(string);
 		System.out.println("-----------");
