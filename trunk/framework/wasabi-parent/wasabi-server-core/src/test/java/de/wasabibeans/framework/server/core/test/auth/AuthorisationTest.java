efb44b21fe0c0e2299b41f679cca871ffb8ec85f
d.klaholt@gmx.de <d.klaholt@gmx.de@4fd6c35a-9cae-1b4d-042b-f8107d73f882>
2010-06-20 22:27:19 +0000

    Removed embedded jboss again. The embedded jboss project is no longer supported (see http://community.jboss.org/wiki/embeddedjboss). Accordingly problems due to lack of support begin to appear (see http://community.jboss.org/thread/18324). Instead the Apache Arquillian project (http://www.jboss.org/arquillian) gains popularity. I will integrate that one soon.

--- trunk/framework/wasabi-parent/wasabi-server-core/src/test/java/de/wasabibeans/framework/server/core/test/auth/AuthorisationTest.java
+++ trunk/framework/wasabi-parent/wasabi-server-core/src/test/java/de/wasabibeans/framework/server/core/test/auth/AuthorisationTest.java
@@ -3,30 +3,22 @@ package de.wasabibeans.framework.server.core.test.auth;
 import javax.naming.NamingException;
 import javax.security.auth.login.LoginException;
 
-import org.jboss.embedded.Bootstrap;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import de.wasabibeans.framework.server.core.common.WasabiConnection;
 
 public class AuthorisationTest {
 	
-	@BeforeClass
-	public static void startJBoss() throws Exception {
-		Bootstrap stiefelriemen = Bootstrap.getInstance();
-		stiefelriemen.bootstrap();
-		
-	}
 
-/*	@Before
+	@Before
 	public void setUp() throws LoginException, NamingException {
 
 		WasabiConnection wasabiConnection = new WasabiConnection();
 		wasabiConnection.connect("localhost", "1099");
 		wasabiConnection.login("root", "meerrettich");
 	}
-*/
+
 	@Test
 	public void test() {
 		assert 1 == 1;
