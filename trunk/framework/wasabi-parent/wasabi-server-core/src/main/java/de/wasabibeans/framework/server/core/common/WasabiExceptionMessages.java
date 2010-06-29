5981a3b1682714a456cf2367fb567edcf5a9c66f
d.klaholt@gmx.de <d.klaholt@gmx.de@4fd6c35a-9cae-1b4d-042b-f8107d73f882>
2010-06-29 11:44:28 +0000

    added method for parametrized strings

--- trunk/framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/common/WasabiExceptionMessages.java
+++ trunk/framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/common/WasabiExceptionMessages.java
@@ -21,6 +21,13 @@ package de.wasabibeans.framework.server.core.common;
 
 public class WasabiExceptionMessages {
 
+	public static String get(String msg, String... params) {
+		for (int i = 0; i < params.length; i++) {
+			msg = msg.replaceFirst("&" + i, params[i]);
+		}
+		return msg;
+	}
+
 	/* UsernamePasswordPrincipal */
 	public final static String USERNAME_PASSWORD_PRINCIPAL_NULL_NAME = "Name cannot be null.";
 
@@ -33,14 +40,14 @@ public class WasabiExceptionMessages {
 	public final static String TRANSFER_DTO2NODE_NULLSESSION = "Attempted to convert DTO to internal respresentation without a given JCR session.";
 	public final static String TRANSFER_NODE2DTO_NULLNODE = "Attempted to convert null to DTO.";
 	public final static String TRANSFER_NODE2DTO_REFLECTERROR = "Could not create corresponding DTO for given internal representation.";
-	
+
 	/* JNDI */
 	public final static String JNDI_NO_CONTEXT = "Could not initialize JNDI context";
-	
+
 	/* JCR */
 	public final static String JCR_REPOSITORY_FAILURE = "Could not access the JCR repository.";
 	public final static String JCR_LOGIN_FAILURE = "Could not establish JCR session. Login failed.";
-	
+
 	/* Internal Services */
 	public final static String INTERNAL_NAME_NULL = "The given name must not be null.";
 	public final static String INTERNAL_ENVIRONMENT_NULL = "The given environment must not be null.";
