9b1c3f0ded842780131e7deaa84663ddbc1669ce
janniss <janniss@upb.de>
2010-07-15 14:57:50 +0000

    fixes

---
+++ framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/authorization/WasabiUserACL.java
@@ -0,0 +1,24 @@
+package de.wasabibeans.framework.server.core.authorization;
+
+import javax.jcr.Node;
+
+import de.wasabibeans.framework.server.core.common.WasabiPermission;
+import de.wasabibeans.framework.server.core.exception.UnexpectedInternalProblemException;
+import de.wasabibeans.framework.server.core.internal.ACLServiceImpl;
+
+public class WasabiUserACL {
+
+	public static void ACLEntryForCreate(Node userNode, Node homeRoomNode, Node callerPrincipalNode, String callerPrincipal)
+			throws UnexpectedInternalProblemException {
+		int[] rights = { WasabiPermission.VIEW, WasabiPermission.READ, WasabiPermission.INSERT, WasabiPermission.WRITE,
+				WasabiPermission.EXECUTE, WasabiPermission.COMMENT, WasabiPermission.GRANT };
+		boolean[] allow = { true, true, true, true, true, true, true };
+
+		ACLServiceImpl.create(userNode, userNode, rights, allow, 0, 0);
+		ACLServiceImpl.create(homeRoomNode, userNode, rights, allow, 0, 0);
+
+		// TODO: deactivate inheritance; check if callerPrincipal is in admin group
+		if (callerPrincipalNode != userNode && !callerPrincipal.equals("root"))
+			ACLServiceImpl.create(userNode, callerPrincipalNode, rights, allow, 0, 0);
+	}
+}
