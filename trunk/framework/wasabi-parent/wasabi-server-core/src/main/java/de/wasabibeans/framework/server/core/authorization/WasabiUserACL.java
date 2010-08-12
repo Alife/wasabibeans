efa6003acac62c4db14a650964268e6fe1cc5815
janniss@upb.de <janniss@upb.de@4fd6c35a-9cae-1b4d-042b-f8107d73f882>
2010-08-12 07:05:01 +0000

    fix

--- trunk/framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/authorization/WasabiUserACL.java
+++ trunk/framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/authorization/WasabiUserACL.java
@@ -27,11 +27,12 @@ import javax.jcr.Session;
 import de.wasabibeans.framework.server.core.common.WasabiPermission;
 import de.wasabibeans.framework.server.core.exception.UnexpectedInternalProblemException;
 import de.wasabibeans.framework.server.core.internal.ACLServiceImpl;
+import de.wasabibeans.framework.server.core.internal.UserServiceImpl;
 
 public class WasabiUserACL {
 
-	public static void ACLEntryForCreate(Node userNode, Node homeRoomNode, Node callerPrincipalNode,
-			String callerPrincipal, Session s) throws UnexpectedInternalProblemException {
+	public static void ACLEntryForCreate(Node userNode, Node homeRoomNode, String callerPrincipal, Session s)
+			throws UnexpectedInternalProblemException {
 		int[] rights = { WasabiPermission.VIEW, WasabiPermission.READ, WasabiPermission.INSERT, WasabiPermission.WRITE,
 				WasabiPermission.EXECUTE, WasabiPermission.COMMENT, WasabiPermission.GRANT };
 		boolean[] allow = { true, true, true, true, true, true, true };
@@ -40,7 +41,10 @@ public class WasabiUserACL {
 		ACLServiceImpl.create(homeRoomNode, userNode, rights, allow, 0, 0, s);
 
 		// TODO: deactivate inheritance; check if callerPrincipal is in admin group
-		if (callerPrincipalNode != userNode && !callerPrincipal.equals("root"))
-			ACLServiceImpl.create(userNode, callerPrincipalNode, rights, allow, 0, 0, s);
+		if (!callerPrincipal.equals("root")) {
+			Node callerPrincipalNode = UserServiceImpl.getUserByName(callerPrincipal, s);
+			if (callerPrincipalNode != userNode)
+				ACLServiceImpl.create(userNode, callerPrincipalNode, rights, allow, 0, 0, s);
+		}
 	}
 }
