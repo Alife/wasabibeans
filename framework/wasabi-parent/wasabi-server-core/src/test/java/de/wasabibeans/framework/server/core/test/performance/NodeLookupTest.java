7db9975dd9bbfa0bc9679b1e566cf675ecf998a5
d.klaholt <d.klaholt@gmx.de>
2010-08-16 16:53:22 +0000

    fix

--- framework/wasabi-parent/wasabi-server-core/src/test/java/de/wasabibeans/framework/server/core/test/performance/NodeLookupTest.java
+++ framework/wasabi-parent/wasabi-server-core/src/test/java/de/wasabibeans/framework/server/core/test/performance/NodeLookupTest.java
@@ -19,39 +19,44 @@ package de.wasabibeans.framework.server.core.test.performance;
  *
  *  Further information are online available at: http://www.wasabibeans.de
  */
-//package de.wasabibeans.framework.server.core.test.remote;
-//
-//import java.util.Vector;
-//
-//import org.jboss.arquillian.api.Run;
-//import org.jboss.arquillian.api.RunModeType;
-//import org.testng.annotations.BeforeMethod;
-//import org.testng.annotations.Test;
-//
-//@Run(RunModeType.AS_CLIENT)
-//public class NodeLookupTest extends WasabiRemoteTest {
-//	
-//	@BeforeMethod
-//	public void setUpBeforeEachMethod() throws Exception {
-//		// initialize jcr repository
-//		testhelper.initRepository();
-//	}
-//	
-//	@Test
-//	public void idLookup() throws Exception {
-//		Vector<String> nodeIds = testhelper.createManyNodes(10000);
-//		Vector<String> result = testhelper.getManyNodesByIdLookup(nodeIds);
-//		System.out.println(result.size());
-//		System.out.println(result.lastElement());
-//	
-//	}
-//	
-//	@Test
-//	public void idFilter() throws Exception {
-//		Vector<String> nodeIds = testhelper.createManyNodes(10000);
-//		Vector<String> result = testhelper.getManyNodesByIdFilter(nodeIds);
-//		System.out.println(result.size());
-//		System.out.println(result.lastElement());
-//	}
-//
-//}
+
+import java.util.Vector;
+
+import org.jboss.arquillian.api.Run;
+import org.jboss.arquillian.api.RunModeType;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import de.wasabibeans.framework.server.core.test.remote.WasabiRemoteTest;
+import de.wasabibeans.framework.server.core.test.testhelper.TestHelperRemote;
+
+@Run(RunModeType.AS_CLIENT)
+public class NodeLookupTest extends WasabiRemoteTest {
+	
+	private TestHelperRemote testhelper;
+	
+	@BeforeMethod
+	public void setUpBeforeEachMethod() throws Exception {
+		testhelper = (TestHelperRemote) reWaCon.lookup("TestHelper");
+		testhelper.initDatabase();
+		rootRoom = testhelper.initRepository();
+	}
+	
+	@Test
+	public void idLookup() throws Exception {
+		Vector<String> nodeIds = testhelper.createManyNodes(10000);
+		Vector<String> result = testhelper.getManyNodesByIdLookup(nodeIds);
+		System.out.println(result.size());
+		System.out.println(result.lastElement());
+	
+	}
+	
+	@Test
+	public void idFilter() throws Exception {
+		Vector<String> nodeIds = testhelper.createManyNodes(10000);
+		Vector<String> result = testhelper.getManyNodesByIdFilter(nodeIds);
+		System.out.println(result.size());
+		System.out.println(result.lastElement());
+	}
+
+}
