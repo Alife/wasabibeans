f6fff78e5179314e2b3402cd74a3fa874e564b75
d.klaholt <d.klaholt@gmx.de>
2010-08-21 16:34:09 +0000

    done

---
+++ framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/util/LockingHelper.java
@@ -0,0 +1,77 @@
+/* 
+ * Copyright (C) 2010 
+ * Jonas Schulte, Dominik Klaholt, Jannis Sauer
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU GENERAL PUBLIC LICENSE as published by
+ *  the Free Software Foundation; either version 3 of the license, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU GENERAL PUBLIC LICENSE (GPL) for more details.
+ *
+ *  You should have received a copy of the GNU GENERAL PUBLIC LICENSE version 3
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ *  Further information are online available at: http://www.wasabibeans.de
+ */
+
+package de.wasabibeans.framework.server.core.util;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.jcr.AccessDeniedException;
+import javax.jcr.InvalidItemStateException;
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.jcr.lock.LockException;
+import javax.jcr.lock.LockManager;
+
+import de.wasabibeans.framework.server.core.exception.UnexpectedInternalProblemException;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+public class LockingHelper implements LockingHelperLocal {
+
+	private JcrConnector jcr;
+
+	public LockingHelper() {
+		this.jcr = JcrConnector.getJCRConnector();
+	}
+
+	@Override
+	public String acquireLock(Node node) throws AccessDeniedException, LockException, PathNotFoundException,
+			InvalidItemStateException, UnsupportedRepositoryOperationException, RepositoryException,
+			UnexpectedInternalProblemException {
+		Session s = jcr.getJCRSession();
+		try {
+			LockManager lockManager = s.getWorkspace().getLockManager();
+			String lockToken = lockManager.lock(node.getPath(), false, false, 10, null).getLockToken();
+			lockManager.removeLockToken(lockToken);
+			return lockToken;
+		} finally {
+			s.logout();
+		}
+	}
+
+	@Override
+	public void releaseLock(Node node, String lockToken) throws AccessDeniedException, LockException,
+			PathNotFoundException, InvalidItemStateException, UnsupportedRepositoryOperationException,
+			RepositoryException, UnexpectedInternalProblemException {
+		Session s = jcr.getJCRSession();
+		try {
+			LockManager lockManager = s.getWorkspace().getLockManager();
+			lockManager.addLockToken(lockToken);
+			lockManager.unlock(node.getPath());
+		} finally {
+			s.logout();
+		}
+	}
+}
