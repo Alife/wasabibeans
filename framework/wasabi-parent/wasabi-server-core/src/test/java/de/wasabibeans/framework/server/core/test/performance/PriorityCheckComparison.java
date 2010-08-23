499163a500aea7e6e5e7d3af9dd72d9245de6bc3
janniss <janniss@upb.de>
2010-08-23 18:58:59 +0000

    some fixes and optimizations

--- framework/wasabi-parent/wasabi-server-core/src/test/java/de/wasabibeans/framework/server/core/test/performance/PriorityCheckComparison.java
+++ framework/wasabi-parent/wasabi-server-core/src/test/java/de/wasabibeans/framework/server/core/test/performance/PriorityCheckComparison.java
@@ -1,6 +1,5 @@
 package de.wasabibeans.framework.server.core.test.performance;
 
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Vector;
 
@@ -9,9 +8,6 @@ import org.jboss.arquillian.api.RunModeType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Statement;
 
 import de.wasabibeans.framework.server.core.common.WasabiPermission;
 import de.wasabibeans.framework.server.core.dto.WasabiRoomDTO;
@@ -41,36 +37,6 @@ public class PriorityCheckComparison extends WasabiRemoteTest {
 
 	@Test
 	public void createTest() throws WasabiException, SQLException, ClassNotFoundException {
-
-//		final String dbDrv = "com.mysql.jdbc.jdbc2.optional.MysqlXADataSource";
-//		final String dbUrl = "jdbc:mysql://localhost:3306/wasabibeans";
-//		final String dbUsr = "wasabi";
-//		final String dbPwd = "wasabi";
-//		final String dbTyp = "ENGINE=InnoDB";
-//		final String dbTbl = "MeineTestTabelle1";
-//
-//		Connection cn = null;
-//		Statement st = null;
-//		try {
-//			Class.forName(dbDrv);
-//			cn = (Connection) DriverManager.getConnection(dbUrl, dbUsr, dbPwd);
-//			cn.setAutoCommit(false);
-//			System.out.println("AutoCommit=" + cn.getAutoCommit() + ", TransactionIsolation="
-//					+ cn.getTransactionIsolation());
-//
-//		} finally {
-//			try {
-//				if (null != st)
-//					st.close();
-//			} catch (Exception ex) {
-//			}
-//			try {
-//				if (null != cn)
-//					cn.close();
-//			} catch (Exception ex) {
-//			}
-//		}
-
 		// Create user
 		WasabiUserDTO user = userService().create("testUser", "password");
 		WasabiUserDTO loginUser = userService().getUserByName("user");
@@ -95,9 +61,9 @@ public class PriorityCheckComparison extends WasabiRemoteTest {
 		long start1 = System.currentTimeMillis();
 
 		Vector<WasabiRoomDTO> rooms = roomService().getRooms(usersHome);
-//		for (WasabiRoomDTO wasabiRoomDTO : rooms) {
-//			roomService().getName(wasabiRoomDTO);
-//		}
+		// for (WasabiRoomDTO wasabiRoomDTO : rooms) {
+		// roomService().getName(wasabiRoomDTO);
+		// }
 
 		long end1 = System.currentTimeMillis();
 
