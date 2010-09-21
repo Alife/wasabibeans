afb32eca12482de2c16588712f99d6e1621ca0e2
janniss@upb.de <janniss@upb.de@4fd6c35a-9cae-1b4d-042b-f8107d73f882>
2010-09-21 11:33:45 +0000

    simple performance test

---
+++ trunk/framework/wasabi-parent/wasabi-server-core/src/test/java/de/wasabibeans/framework/server/core/test/performance/SimpleRightsPerformanceTest.java
@@ -0,0 +1,111 @@
+/* 
+ * Copyright (C) 2010 
+ * Jonas Schulte, Dominik Klaholt, Jannis Sauer
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU GENERAL PUBLIC LICENSE as published by
+ *  the Free Software Foundation; either version 3 of the license, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU GENERAL PUBLIC LICENSE (GPL) for more details.
+ *
+ *  You should have received a copy of the GNU GENERAL PUBLIC LICENSE version 3
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ *  Further information are online available at: http://www.wasabibeans.de
+ */
+
+package de.wasabibeans.framework.server.core.test.performance;
+
+import org.jboss.arquillian.api.Run;
+import org.jboss.arquillian.api.RunModeType;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import de.wasabibeans.framework.server.core.dto.WasabiRoomDTO;
+import de.wasabibeans.framework.server.core.dto.WasabiUserDTO;
+import de.wasabibeans.framework.server.core.exception.WasabiException;
+import de.wasabibeans.framework.server.core.test.remote.WasabiRemoteTest;
+import de.wasabibeans.framework.server.core.test.testhelper.TestHelperRemote;
+
+@Run(RunModeType.AS_CLIENT)
+public class SimpleRightsPerformanceTest extends WasabiRemoteTest {
+
+	@BeforeMethod
+	public void setUpBeforeEachMethod() throws Exception {
+		// initialize test
+		TestHelperRemote testhelper = (TestHelperRemote) reWaCon.lookup("TestHelper");
+		testhelper.initDatabase();
+		rootRoom = testhelper.initRepository();
+		testhelper.initTestUser();
+
+		reWaCon.login("user", "user");
+	}
+
+	@AfterMethod
+	public void tearDownAfterEachMethod() throws Exception {
+		reWaCon.logout();
+	}
+
+	@Test
+	public void createRooms1() throws WasabiException {
+		int numberOfRooms = 100;
+
+		WasabiUserDTO user = userService().getUserByName("user");
+		WasabiRoomDTO usersHome = userService().getHomeRoom(user).getValue();
+
+		long startTime = java.lang.System.currentTimeMillis();
+
+		for (long i = startTime; i < (startTime + numberOfRooms); i++) {
+			roomService().create(String.valueOf(i), usersHome);
+			// System.out.println("create room " + i);
+		}
+
+		long endTime = java.lang.System.currentTimeMillis();
+
+		System.out.println("Pass 1: " + (endTime - startTime));
+	}
+
+	@Test
+	public void createRooms2() throws WasabiException {
+		int numberOfRooms = 100;
+
+		WasabiUserDTO user = userService().getUserByName("user");
+		WasabiRoomDTO usersHome = userService().getHomeRoom(user).getValue();
+
+		long startTime = java.lang.System.currentTimeMillis();
+
+		for (long i = startTime; i < (startTime + numberOfRooms); i++) {
+			roomService().create(String.valueOf(i), usersHome);
+			// System.out.println("create room " + i);
+		}
+
+		long endTime = java.lang.System.currentTimeMillis();
+
+		System.out.println("Pass 2: " + (endTime - startTime));
+	}
+
+	@Test
+	public void createRooms3() throws WasabiException {
+		int numberOfRooms = 100;
+
+		WasabiUserDTO user = userService().getUserByName("user");
+		WasabiRoomDTO usersHome = userService().getHomeRoom(user).getValue();
+
+		long startTime = java.lang.System.currentTimeMillis();
+
+		for (long i = startTime; i < (startTime + numberOfRooms); i++) {
+			roomService().create(String.valueOf(i), usersHome);
+			// System.out.println("create room " + i);
+		}
+
+		long endTime = java.lang.System.currentTimeMillis();
+
+		System.out.println("Pass 3: " + (endTime - startTime));
+	}
+}
