9766c87d61b3594247a22f1d1d799a8ca16b26a7
janniss@upb.de <janniss@upb.de@4fd6c35a-9cae-1b4d-042b-f8107d73f882>
2010-10-04 17:40:32 +0000

    add ACL support to VersioningService + test cases

--- trunk/framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/bean/VersioningService.java
+++ trunk/framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/bean/VersioningService.java
@@ -5,6 +5,8 @@ import java.util.Vector;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
@@ -20,13 +22,17 @@ import org.jboss.ejb3.annotation.SecurityDomain;
 
 import de.wasabibeans.framework.server.core.aop.JCRSessionInterceptor;
 import de.wasabibeans.framework.server.core.aop.WasabiAOP;
+import de.wasabibeans.framework.server.core.authorization.WasabiAuthorizer;
+import de.wasabibeans.framework.server.core.common.WasabiConstants;
 import de.wasabibeans.framework.server.core.common.WasabiExceptionMessages;
+import de.wasabibeans.framework.server.core.common.WasabiPermission;
 import de.wasabibeans.framework.server.core.dto.TransferManager;
 import de.wasabibeans.framework.server.core.dto.WasabiDocumentDTO;
 import de.wasabibeans.framework.server.core.dto.WasabiLocationDTO;
 import de.wasabibeans.framework.server.core.dto.WasabiObjectDTO;
 import de.wasabibeans.framework.server.core.dto.WasabiVersionDTO;
 import de.wasabibeans.framework.server.core.exception.ConcurrentModificationException;
+import de.wasabibeans.framework.server.core.exception.NoPermissionException;
 import de.wasabibeans.framework.server.core.exception.ObjectDoesNotExistException;
 import de.wasabibeans.framework.server.core.exception.UnexpectedInternalProblemException;
 import de.wasabibeans.framework.server.core.internal.VersioningServiceImpl;
@@ -41,26 +47,62 @@ import de.wasabibeans.framework.server.core.util.JndiConnector;
 @Interceptors( { JCRSessionInterceptor.class })
 public class VersioningService implements VersioningServiceLocal, VersioningServiceRemote, WasabiAOP {
 
-	protected JndiConnector jndi;
+	@Resource
+	protected SessionContext ctx;
+
 	protected JcrConnector jcr;
+	protected JndiConnector jndi;
 
-	@PostConstruct
-	public void postConstruct() {
-		this.jndi = JndiConnector.getJNDIConnector();
-		this.jcr = JcrConnector.getJCRConnector(jndi);
-	}
+	/**
+	 * Creates a new version for the wasabi-object represented by the given {@code WasabiObjectDTO} {@code dto}. If the
+	 * given wasabi-object has a subtree that contains further versionable wasabi-objects, then a new version for each
+	 * of these versionable wasabi-objects will be created as well. The entire subtree of the given wasabi-object will
+	 * be locked during this method. All versions created by this method will have the same comment and the same label.
+	 * 
+	 * @param object
+	 * @param comment
+	 * @throws UnexpectedInternalProblemException
+	 * @throws ObjectDoesNotExistException
+	 * @throws ConcurrentModificationException
+	 * @throws NoPermissionException
+	 */
+	public void createVersion(WasabiObjectDTO object, String comment) throws UnexpectedInternalProblemException,
+			ObjectDoesNotExistException, ConcurrentModificationException, NoPermissionException {
+		if (!(object instanceof WasabiLocationDTO || object instanceof WasabiDocumentDTO)) {
+			throw new IllegalArgumentException(WasabiExceptionMessages.VERSIONING_NOT_SUPPORTED);
+		}
 
-	@PreDestroy
-	public void preDestroy() {
-		jndi.close();
-	}
+		Node objectNode = null;
+		Session s = jcr.getJCRSession();
+		try {
+			objectNode = TransferManager.convertDTO2Node(object, s);
+			String callerPrincipal = ctx.getCallerPrincipal().getName();
 
-	public JndiConnector getJndiConnector() {
-		return jndi;
+			/* Authorization - Begin */
+			if (WasabiConstants.ACL_CHECK_ENABLE)
+				if (!WasabiAuthorizer.authorize(objectNode, callerPrincipal, WasabiPermission.WRITE, s))
+					throw new NoPermissionException(WasabiExceptionMessages.get(
+							WasabiExceptionMessages.AUTHORIZATION_NO_PERMISSION, "VersionService.createVersions()",
+							"WRITE", "object"));
+			/* Authorization - End */
+
+			// get unique version label (that is, unique in the version histories of the affected nodes)
+			// timestamp
+			String label = "" + Calendar.getInstance().getTimeInMillis();
+			// create versions for entire subtree
+			VersioningServiceImpl.createVersionRecursively(objectNode, label, comment, s.getWorkspace()
+					.getVersionManager());
+		} catch (RepositoryException re) {
+			throw new UnexpectedInternalProblemException(WasabiExceptionMessages.JCR_REPOSITORY_FAILURE, re);
+		}
 	}
 
 	public JcrConnector getJcrConnector() {
 		return jcr;
+	}
+
+	public JndiConnector getJndiConnector() {
+		return jndi;
 	}
 
 	/**
@@ -72,17 +114,29 @@ public class VersioningService implements VersioningServiceLocal, VersioningServ
 	 * @return {@code Vector} of {@code WasabiVersionDTO}s
 	 * @throws UnexpectedInternalProblemException
 	 * @throws ObjectDoesNotExistException
+	 * @throws NoPermissionException
 	 */
-	public Vector<WasabiVersionDTO> getVersions(WasabiObjectDTO dto) throws UnexpectedInternalProblemException,
-			ObjectDoesNotExistException {
-		if (!(dto instanceof WasabiLocationDTO || dto instanceof WasabiDocumentDTO)) {
+	public Vector<WasabiVersionDTO> getVersions(WasabiObjectDTO object) throws UnexpectedInternalProblemException,
+			ObjectDoesNotExistException, NoPermissionException {
+		if (!(object instanceof WasabiLocationDTO || object instanceof WasabiDocumentDTO)) {
 			throw new IllegalArgumentException(WasabiExceptionMessages.VERSIONING_NOT_SUPPORTED);
 		}
 
 		Session s = jcr.getJCRSession();
 		try {
-			Node node = TransferManager.convertDTO2Node(dto, s);
-			VersionHistory versionHistory = s.getWorkspace().getVersionManager().getVersionHistory(node.getPath());
+			Node objectNode = TransferManager.convertDTO2Node(object, s);
+			String callerPrincipal = ctx.getCallerPrincipal().getName();
+
+			/* Authorization - Begin */
+			if (WasabiConstants.ACL_CHECK_ENABLE)
+				if (!WasabiAuthorizer.authorize(objectNode, callerPrincipal, WasabiPermission.READ, s))
+					throw new NoPermissionException(WasabiExceptionMessages.get(
+							WasabiExceptionMessages.AUTHORIZATION_NO_PERMISSION, "VersionService.getVersions()",
+							"READ", "object"));
+			/* Authorization - End */
+
+			VersionHistory versionHistory = s.getWorkspace().getVersionManager()
+					.getVersionHistory(objectNode.getPath());
 			String rootVersionName = versionHistory.getRootVersion().getName();
 			Vector<WasabiVersionDTO> versions = new Vector<WasabiVersionDTO>();
 			for (VersionIterator vi = versionHistory.getAllVersions(); vi.hasNext();) {
@@ -98,36 +152,15 @@ public class VersioningService implements VersioningServiceLocal, VersioningServ
 		}
 	}
 
-	/**
-	 * Creates a new version for the wasabi-object represented by the given {@code WasabiObjectDTO} {@code dto}. If the
-	 * given wasabi-object has a subtree that contains further versionable wasabi-objects, then a new version for each
-	 * of these versionable wasabi-objects will be created as well. The entire subtree of the given wasabi-object will
-	 * be locked during this method. All versions created by this method will have the same comment and the same label.
-	 * 
-	 * @param dto
-	 * @param comment
-	 * @throws UnexpectedInternalProblemException
-	 * @throws ObjectDoesNotExistException
-	 * @throws ConcurrentModificationException
-	 */
-	public void createVersion(WasabiObjectDTO dto, String comment) throws UnexpectedInternalProblemException,
-			ObjectDoesNotExistException, ConcurrentModificationException {
-		if (!(dto instanceof WasabiLocationDTO || dto instanceof WasabiDocumentDTO)) {
-			throw new IllegalArgumentException(WasabiExceptionMessages.VERSIONING_NOT_SUPPORTED);
-		}
+	@PostConstruct
+	public void postConstruct() {
+		this.jndi = JndiConnector.getJNDIConnector();
+		this.jcr = JcrConnector.getJCRConnector(jndi);
+	}
 
-		Node node = null;
-		Session s = jcr.getJCRSession();
-		try {
-			node = TransferManager.convertDTO2Node(dto, s);
-			// get unique version label (that is, unique in the version histories of the affected nodes)
-			// timestamp
-			String label = "" + Calendar.getInstance().getTimeInMillis();
-			// create versions for entire subtree
-			VersioningServiceImpl.createVersionRecursively(node, label, comment, s.getWorkspace().getVersionManager());
-		} catch (RepositoryException re) {
-			throw new UnexpectedInternalProblemException(WasabiExceptionMessages.JCR_REPOSITORY_FAILURE, re);
-		}
+	@PreDestroy
+	public void preDestroy() {
+		jndi.close();
 	}
 
 	/**
@@ -137,24 +170,36 @@ public class VersioningService implements VersioningServiceLocal, VersioningServ
 	 * versionable wasabi-objects as well. The entire subtree of the given wasabi-object will be locked during this
 	 * method.
 	 * 
-	 * @param dto
+	 * @param object
 	 * @param versionLabel
 	 * @throws UnexpectedInternalProblemException
 	 * @throws ObjectDoesNotExistException
 	 * @throws ConcurrentModificationException
+	 * @throws NoPermissionException
 	 */
-	public void restoreVersion(WasabiObjectDTO dto, String versionLabel) throws UnexpectedInternalProblemException,
-			ObjectDoesNotExistException, ConcurrentModificationException {
-		if (!(dto instanceof WasabiLocationDTO || dto instanceof WasabiDocumentDTO)) {
+	public void restoreVersion(WasabiObjectDTO object, String versionLabel) throws UnexpectedInternalProblemException,
+			ObjectDoesNotExistException, ConcurrentModificationException, NoPermissionException {
+		if (!(object instanceof WasabiLocationDTO || object instanceof WasabiDocumentDTO)) {
 			throw new IllegalArgumentException(WasabiExceptionMessages.VERSIONING_NOT_SUPPORTED);
 		}
 
-		Node node = null;
+		Node objectNode = null;
 		Session s = jcr.getJCRSession();
 		try {
-			node = TransferManager.convertDTO2Node(dto, s);
+			objectNode = TransferManager.convertDTO2Node(object, s);
+			String callerPrincipal = ctx.getCallerPrincipal().getName();
+
+			/* Authorization - Begin */
+			if (WasabiConstants.ACL_CHECK_ENABLE)
+				if (!WasabiAuthorizer.authorize(objectNode, callerPrincipal, WasabiPermission.WRITE, s))
+					throw new NoPermissionException(WasabiExceptionMessages.get(
+							WasabiExceptionMessages.AUTHORIZATION_NO_PERMISSION, "VersionService.restoreVersions()",
+							"WRITE", "object"));
+			/* Authorization - End */
+
 			// restore versions for entire subtree
-			VersioningServiceImpl.restoreVersionRecursively(node, versionLabel, s.getWorkspace().getVersionManager());
+			VersioningServiceImpl.restoreVersionRecursively(objectNode, versionLabel, s.getWorkspace()
+					.getVersionManager());
 		} catch (RepositoryException re) {
 			throw new UnexpectedInternalProblemException(WasabiExceptionMessages.JCR_REPOSITORY_FAILURE, re);
 		}
--- trunk/framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/local/VersioningServiceLocal.java
+++ trunk/framework/wasabi-parent/wasabi-server-core/src/main/java/de/wasabibeans/framework/server/core/local/VersioningServiceLocal.java
@@ -7,18 +7,19 @@ import javax.ejb.Local;
 import de.wasabibeans.framework.server.core.dto.WasabiObjectDTO;
 import de.wasabibeans.framework.server.core.dto.WasabiVersionDTO;
 import de.wasabibeans.framework.server.core.exception.ConcurrentModificationException;
+import de.wasabibeans.framework.server.core.exception.NoPermissionException;
 import de.wasabibeans.framework.server.core.exception.ObjectDoesNotExistException;
 import de.wasabibeans.framework.server.core.exception.UnexpectedInternalProblemException;
 
 @Local
 public interface VersioningServiceLocal {
 
-	public Vector<WasabiVersionDTO> getVersions(WasabiObjectDTO dto) throws UnexpectedInternalProblemException,
-			ObjectDoesNotExistException;
+	public void createVersion(WasabiObjectDTO object, String comment) throws UnexpectedInternalProblemException,
+			ObjectDoesNotExistException, ConcurrentModificationException, NoPermissionException;
 
-	public void createVersion(WasabiObjectDTO dto, String comment) throws UnexpectedInternalProblemException,
-			ObjectDoesNotExistException, ConcurrentModificationException;
+	public Vector<WasabiVersionDTO> getVersions(WasabiObjectDTO object) throws UnexpectedInternalProblemException,
+			ObjectDoesNotExistException, NoPermissionException;
 
-	public void restoreVersion(WasabiObjectDTO dto, String versionLabel) throws UnexpectedInternalProblemException,
-			ObjectDoesNotExistException, ConcurrentModificationException;
+	public void restoreVersion(WasabiObjectDTO object, String versionLabel) throws UnexpectedInternalProblemException,
+			ObjectDoesNotExistException, ConcurrentModificationException, NoPermissionException;
 }
